d926d54e8516ddcc2748021e2699ee67a102b5bf
IlyaChibirev <chibirevspartak@gamail.com>
2022-04-27 02:13:58 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static com.company.Education.HIGHER;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+      long age = persons.stream()
+              .filter(x -> x.getAge() < 18)
+              .count();
+        System.out.println("Количество несовершеннолетних: " + age);
+
+      List<String> conscript = persons.stream()
+              .filter(x -> x.getAge() >= 18 && x.getAge() <= 27)
+              .filter(x -> x.getSex() == Sex.MAN && x.getSex() != Sex.WOMAN)
+              .map(Person::getFamily)
+              .collect(Collectors.toList());
+      System.out.println(conscript);
+
+      List<Person> workPeople = persons.stream()
+              .filter(x -> x.getEducation() == Education.HIGHER)
+              .filter(x -> x.getAge() >= 18 && x.getAge() <= 65 && x.getSex() == Sex.MAN ||
+                      x.getAge() >= 18 && x.getAge() <= 60 && x.getSex() == Sex.WOMAN)
+              .sorted(Comparator.comparing(Person::getFamily))
+              .collect(Collectors.toList());
+      System.out.println(workPeople);
+    }
+}
